4e783848f99730e4759d9e9f2d8ba4a66ad6bd15
Ilham045 <147364338+Ilham045@users.noreply.github.com>
2023-11-10 18:45:11 +0700

    Update index.blade.php

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -4,7 +4,6 @@
   
 @section('contents')
     <div class="d-flex align-items-center justify-content-between">
-<<<<<<< HEAD
         <h1 class="mb-0">Daftar Kontak</h1>
         <h1 class="mb-0">List Contact</h1>
         <a href="{{ route('contact.create') }}" class="btn btn-primary">Add Kontak</a>
